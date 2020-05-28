e407da5d740d29f5096cccd7c2c73e495cc129ee
Christopher Orr <chris@orr.me.uk>
2020-05-28 19:28:54 +0200

    Fix encoding issues when the HMAC contains one-or-more null bytes.
    I'm not sure why there was so much complexity surrounding passing values
    into each function, but it was causing issues whenever the output of the
    HMAC, which is binary, contained null bytes.
    
    Null bytes can't be represented by a Bash string, so we would see:
      -bash: warning: command substitution: ignored null byte in input
    
    These would then be ignored, and cause the output to be incorrect.
    
    Now we let each function read directly from stdin, bypassing the need to
    first read the input into a string.

--- README.md
+++ README.md
@@ -7,8 +7,6 @@ The Docker container takes two parameters:
 - the secret used for signing the token
 - the JSON payload
 
-The JWT is printed to stdout. Now and then you'll see some errors that I haven't bothered to figure out (possibly related to base64-encoding without padding?), but they're harmless, and printed to stderr.
-
 The JSON payload can be provided as an argument:
 ```sh
 docker run --rm orrc/jwt-generator my-shared-secret '{"foo": "bar", "num": 1234}'
@@ -18,6 +16,9 @@ Or it can be piped in:
 ```sh
 echo '{"foo": "bar", "num": 1234}' | docker run --rm -i orrc/jwt-generator my-shared-secret
 ```
+
+The JWT is printed to stdout, followed by a newline.
+
 
 ## Credits
 This is just a convenient wrapper around a handy [Bash script][0] created by [@williamhaley][1], including some [shell magic][2] by Filipe Fortes.
--- jwt.sh
+++ jwt.sh
@@ -53,28 +53,23 @@ header=$(
     '
 )
 
-base64_encode()
-{
-    declare input=${1:-$(</dev/stdin)}
+base64_encode() {
     # Use `tr` to URL encode the output from base64.
-    printf '%s' "${input}" | base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'
+    base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'
 }
 
 json() {
-    declare input=${1:-$(</dev/stdin)}
-    printf '%s' "${input}" | jq -c .
+    jq -c .
 }
 
-hmacsha256_sign()
-{
-    declare input=${1:-$(</dev/stdin)}
-    printf '%s' "${input}" | openssl dgst -binary -sha256 -hmac "${secret}"
+hmacsha256_sign() {
+    openssl dgst -binary -sha256 -hmac "${secret}"
 }
 
 header_base64=$(echo "${header}" | json | base64_encode)
 payload_base64=$(echo "${payload}" | json | base64_encode)
 
 header_payload=$(echo "${header_base64}.${payload_base64}")
-signature=$(echo "${header_payload}" | hmacsha256_sign | base64_encode)
+signature=$(echo -n "${header_payload}" | hmacsha256_sign | base64_encode)
 
 echo "${header_payload}.${signature}"
